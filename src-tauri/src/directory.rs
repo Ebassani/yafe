0477a13709ee8a2fc1c454bbb00e5c2610abb347
Eduardo <eduardo.sbassani@gmail.com>
2026-05-06 17:44:41 +0200

    Enhance file metadata retrieval in read_directory function

--- src-tauri/src/directory.rs
+++ src-tauri/src/directory.rs
@@ -1,4 +1,6 @@
+use std::ffi::{OsString};
 use std::fs::{read_dir, FileType};
+use std::os::windows::prelude::MetadataExt;
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde::Serialize;
 use crate::directory::DirFileType::{Dir, File, Symlink};
@@ -7,7 +9,7 @@ pub(crate) fn read_directory(path: &str) -> Result<Vec<FileInfo>, FileError> {
     let dir = read_dir(path).map_err(|message| FileError::DirError(message.to_string()))?;
 
     let mut info: Vec<FileInfo> = Vec::new();
-    
+
     let _ = dir.filter_map(|dir_item| dir_item.ok().map(|file| {
         let metadata = file.metadata().map_err(|err| FileError::MetadataError(err.to_string())).ok().map(|file_metadata|{
             FileMetadata {
@@ -16,17 +18,25 @@ pub(crate) fn read_directory(path: &str) -> Result<Vec<FileInfo>, FileError> {
                 created: file_metadata.created().ok().and_then(system_time_to_u64),
                 file_type: DirFileType::from_file_type(file_metadata.file_type()),
                 modified: file_metadata.modified().ok().and_then(system_time_to_u64),
-                permissions: 0,
-                creation_time: 0,
-                file_attributes: 0,
-                file_size: 0,
-                last_access_time: 0,
-                last_write_time: 0,
+                read_only: file_metadata.permissions().readonly(),
+                creation_time: file_metadata.creation_time(),
+                file_attributes: file_metadata.file_attributes(),
+                file_size: file_metadata.file_size(),
+                last_access_time: file_metadata.last_access_time(),
+                last_write_time: file_metadata.last_write_time(),
             }
         });
 
+        let path = file.path();
+
+        let file_name = match path.file_name() {
+            None => {OsString::from("Name not found")}
+            Some(name) => {name.to_os_string()}
+        };
+
 
         let file = FileInfo {
+            file_name,
             file_metadata: metadata
         };
 
@@ -54,9 +64,9 @@ pub(crate) struct FileMetadata {
     pub(crate) created: Option<u64>,
     pub(crate) file_type: DirFileType,
     pub(crate) modified: Option<u64>,
-    pub(crate) permissions: u64,
+    pub(crate) read_only: bool,
     pub(crate) creation_time: u64,
-    pub(crate) file_attributes: u64,
+    pub(crate) file_attributes: u32,
     pub(crate) file_size: u64,
     pub(crate) last_access_time: u64,
     pub(crate) last_write_time: u64,
@@ -64,6 +74,7 @@ pub(crate) struct FileMetadata {
 
 #[derive(Serialize, Clone)]
 pub(crate) struct FileInfo {
+    pub(crate) file_name: OsString,
     pub(crate) file_metadata: Option<FileMetadata>
 }
 
