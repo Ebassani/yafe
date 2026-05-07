5c47889e5272e4043540960d02865af4f20e748f
Eduardo <eduardo.sbassani@gmail.com>
2026-05-07 14:15:12 +0200

    Refactor read_directory and read_metadata functions to improve file metadata handling and utilize utility functions

--- src-tauri/src/directory/read.rs
+++ src-tauri/src/directory/read.rs
@@ -1,8 +1,10 @@
+use std::path::Path;
 use std::fs;
-use std::fs::{read_dir, Metadata};
+use std::fs::read_dir;
 use crate::directory::errors::FileError;
-use crate::directory::types::{DirFileType, FileInfo, FileMetadata};
-use crate::directory::utils::system_time_to_u64;
+use crate::directory::types::{FileInfo};
+use crate::directory::utils;
+
 
 pub(crate) fn read_directory(dir_path: &str) -> Result<Vec<FileInfo>, FileError> {
     let dir = read_dir(dir_path).map_err(|message| FileError::DirError(message.to_string()))?;
@@ -15,7 +17,7 @@ pub(crate) fn read_directory(dir_path: &str) -> Result<Vec<FileInfo>, FileError>
                 .metadata()
                 .map_err(|err| FileError::MetadataError(err.to_string()))?;
 
-            let file_metadata = fs_metadata_into_file_metadata(metadata);
+            let file_metadata = utils::fs_metadata_into_file_metadata(metadata);
 
             let file_name = file.file_name().to_string_lossy().to_string();
             let path = file.path().to_string_lossy().to_string();
@@ -31,19 +33,19 @@ pub(crate) fn read_directory(dir_path: &str) -> Result<Vec<FileInfo>, FileError>
     info
 }
 
-pub(crate) fn read_metadata(path: &str) -> Result<FileMetadata, FileError> {
+pub(crate) fn read_metadata(path: &str) -> Result<FileInfo, FileError> {
     let metadata = fs::symlink_metadata(path).map_err(|err| FileError::MetadataError(err.to_string()))?;
 
-    Ok(fs_metadata_into_file_metadata(metadata))
-}
+    let file_name = Path::new(path)
+        .file_name()
+        .ok_or_else(|| FileError::PathBuf("Invalid path".into()))?
+        .to_string_lossy()
+        .to_string();
 
-pub(crate) fn fs_metadata_into_file_metadata(metadata: Metadata) -> FileMetadata {
-    FileMetadata {
-        len: metadata.len(),
-        accessed: metadata.accessed().ok().and_then(system_time_to_u64),
-        created: metadata.created().ok().and_then(system_time_to_u64),
-        file_type: DirFileType::from_file_type(metadata.file_type()),
-        modified: metadata.modified().ok().and_then(system_time_to_u64),
-        read_only: metadata.permissions().readonly(),
-    }
+
+    Ok(FileInfo {
+        path: path.to_string(),
+        file_name,
+        file_metadata: utils::fs_metadata_into_file_metadata(metadata),
+    })
 }
--- src-tauri/src/directory/utils.rs
+++ src-tauri/src/directory/utils.rs
@@ -1,5 +1,18 @@
+use std::fs::Metadata;
 use std::time::{SystemTime, UNIX_EPOCH};
+use crate::directory::types::{DirFileType, FileMetadata};
 
 pub(crate) fn system_time_to_u64(time: SystemTime) -> Option<u64> {
     time.duration_since(UNIX_EPOCH).ok().map(|d| d.as_secs())
+}
+
+pub(crate) fn fs_metadata_into_file_metadata(metadata: Metadata) -> FileMetadata {
+    FileMetadata {
+        len: metadata.len(),
+        accessed: metadata.accessed().ok().and_then(system_time_to_u64),
+        created: metadata.created().ok().and_then(system_time_to_u64),
+        file_type: DirFileType::from_file_type(metadata.file_type()),
+        modified: metadata.modified().ok().and_then(system_time_to_u64),
+        read_only: metadata.permissions().readonly(),
+    }
 }
