a113c1f9f0f3f921f1009d717cebf186b3ec6920
Eduardo <eduardo.sbassani@gmail.com>
2026-05-21 15:47:08 +0200

    Add function to index files and their trigrams

--- src-tauri/src/search/indexer.rs
+++ src-tauri/src/search/indexer.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::sync::{Mutex, RwLock};
+use std::sync::atomic::{AtomicU32, Ordering};
 use crate::directory::DirFileType;
 
 /// This file will be a nightmare, I was researching about search types and I found one called trigram search.
@@ -12,7 +13,8 @@ use crate::directory::DirFileType;
 pub(crate) struct Indexer{
     shards: Vec<Mutex<ShardedPostings>>,
     files: RwLock<HashMap<FileId, IndexedEntry>>,
-    file_grams: RwLock<HashMap<FileId, Vec<GramId>>>
+    file_grams: RwLock<HashMap<FileId, Vec<GramId>>>,
+    next_file_id: AtomicU32
 }
 
 const DEFAULT_SHARD_COUNT: usize = 64;
@@ -31,7 +33,45 @@ impl Indexer {
                 .collect(),
             files: Default::default(),
             file_grams: Default::default(),
+            next_file_id: AtomicU32::new(0),
         }
+    }
+
+    pub(crate) fn index_file(&self, name: String, path: String, indexed_entry_kind: IndexedEntryKind) -> FileId {
+        let file_id = self.next_file_id();
+
+        let file_entry = IndexedEntry {
+            path,
+            name: name.clone(),
+            kind: indexed_entry_kind,
+        };
+
+        self.files.write().unwrap().insert(file_id, file_entry);
+
+        let grams = Self::grams_from_text(&name);
+
+        grams.iter().for_each(|&gram| {
+            self.insert_file_gram(gram, file_id)
+        });
+
+        self.file_grams.write().unwrap().insert(file_id, grams);
+
+        file_id
+    }
+
+    fn insert_file_gram(&self, gram_id: GramId, file_id: FileId) {
+        let index = self.shard_index(gram_id);
+        let mut shard = self.shards[index].lock().unwrap();
+
+        shard.postings.entry(gram_id).or_default().push(file_id);
+    }
+
+    fn next_file_id(&self) -> FileId {
+        let id = self.next_file_id.fetch_add(1, Ordering::Relaxed);
+
+        assert_ne!(id, u32::MAX, "file id allocator exhausted");
+
+        FileId(id)
     }
 
     /// This is here to make sure the index used to find a gram is always the same. So, in case a trigram has to be added to the shards, the shard index can be calculated, same thing if it is to retrieve the gram
