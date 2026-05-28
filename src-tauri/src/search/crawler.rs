a9166a7ffbf146b1f10991777a2a124c6c9b8272
Eduardo <eduardo.sbassani@gmail.com>
2026-05-28 12:32:22 +0200

    Refactor CrawlCoordinator to use Mutex for thread-safe error trace management

--- src-tauri/src/search/crawler.rs
+++ src-tauri/src/search/crawler.rs
@@ -1,65 +1,66 @@
 use std::fs::{read_dir};
+use std::sync::Mutex;
 use crate::directory::{dir_entry_into_file_info, DirFileType, FileError, FileErrorType, FileErrorWithPath};
 use crate::queue::Queue;
 use crate::search::indexer::{IndexedEntryKind, Indexer};
 
 pub(crate) struct CrawlCoordinator {
-    error_trace: Vec<FileErrorWithPath>,
+    error_trace: Mutex<Vec<FileErrorWithPath>>,
     queue: Queue<String>
 }
 
 impl CrawlCoordinator {
     pub(crate) fn new() -> Self {
         Self {
-            error_trace: Vec::new(),
+            error_trace: Mutex::new(Vec::new()),
             queue: Queue::new(),
         }
     }
-}
 
-fn crawl(dir_path: &str, indexer: &Indexer, coordinator: &mut CrawlCoordinator, queue: &Queue<String>) {
-    let dir = match read_dir(dir_path).map_err(|message| FileError::directory(message.to_string())) {
-        Ok(read_dir) => {read_dir}
-        Err(err) => {
-            let err_path = FileErrorWithPath::from_file_error_and_path(err, dir_path);
-            coordinator.error_trace.push(err_path);
-
-            return;
-        }
-    };
-    
-    for  dir_item  in dir {
-        match dir_item {
-            Ok(item) => {
-                let file_info = match dir_entry_into_file_info(item) {
-                    Ok(file_info) => {file_info}
-                    Err(err) => {
-                        let err_path = FileErrorWithPath::from_file_error_and_path(err, dir_path);
-                        coordinator.error_trace.push(err_path);
-
-                        continue;
-                    }
-                };
-
-                let file_type = file_info.file_metadata.file_type;
-
-                let is_dir = file_type == DirFileType::Dir;
-
-                indexer.index_file(file_info.file_name.clone(), file_info.path.clone(), IndexedEntryKind::from(file_type));
-
-                if is_dir {
-                    queue.push(file_info.file_name)
-                }
-            }
+    fn crawl_one_directory(&mut self, dir_path: &str, indexer: &Indexer) {
+        let dir = match read_dir(dir_path).map_err(|message| FileError::directory(message.to_string())) {
+            Ok(read_dir) => {read_dir}
             Err(err) => {
-                let file_error = FileError {
-                    error_type: FileErrorType::Directory,
-                    message: err.to_string()
-                };
-                let err_path = FileErrorWithPath::from_file_error_and_path(file_error, dir_path);
-                coordinator.error_trace.push(err_path);
+                let err_path = FileErrorWithPath::from_file_error_and_path(err, dir_path);
+                self.error_trace.lock().unwrap().push(err_path);
 
-                continue;
+                return;
+            }
+        };
+
+        for  dir_item  in dir {
+            match dir_item {
+                Ok(item) => {
+                    let file_info = match dir_entry_into_file_info(item) {
+                        Ok(file_info) => {file_info}
+                        Err(err) => {
+                            let err_path = FileErrorWithPath::from_file_error_and_path(err, dir_path);
+                            self.error_trace.lock().unwrap().push(err_path);
+
+                            continue;
+                        }
+                    };
+
+                    let file_type = file_info.file_metadata.file_type;
+
+                    let is_dir = file_type == DirFileType::Dir;
+
+                    indexer.index_file(file_info.file_name.clone(), file_info.path.clone(), IndexedEntryKind::from(file_type));
+
+                    if is_dir {
+                        self.queue.push(file_info.file_name)
+                    }
+                }
+                Err(err) => {
+                    let file_error = FileError {
+                        error_type: FileErrorType::Directory,
+                        message: err.to_string()
+                    };
+                    let err_path = FileErrorWithPath::from_file_error_and_path(file_error, dir_path);
+                    self.error_trace.lock().unwrap().push(err_path);
+
+                    continue;
+                }
             }
         }
     }
