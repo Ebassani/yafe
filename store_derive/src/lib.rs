b6e3dce51725735480db52e967064ec4d43a0473
Eduardo <eduardo.sbassani@gmail.com>
2026-06-12 14:20:37 +0200

    Getting store attributes

--- store_derive/src/lib.rs
+++ store_derive/src/lib.rs
@@ -1,10 +1,45 @@
 use proc_macro::{TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, LitStr};
+
+#[derive(Default, Debug)]
+struct StoreConfig {
+    table: Option<String>,
+    primary_key: Option<String>
+}
+
+#[derive(Default, Debug)]
+struct FieldConfig {
+    column_name: Option<String>,
+    primary_key: bool,
+    skip: bool
+}
 
 #[proc_macro_derive(Store, attributes(store))]
 pub fn derive_store(token_stream: TokenStream) -> TokenStream {
     let input = parse_macro_input!(token_stream as DeriveInput);
+
+    let attributes = &input.attrs;
+
+    let mut config = StoreConfig::default();
+
+    for attr in attributes {
+        if !attr.path().is_ident("store") { continue; }
+
+        attr.parse_nested_meta(|metadata| {
+            if metadata.path.is_ident("primary_key") {
+                let value = metadata.value()?;
+                let lit: LitStr = value.parse()?;
+                config.table = Some(lit.value());
+                Ok(())
+            } else if metadata.path.is_ident("primary_key") {
+                let value = metadata.value()?;
+                let lit: LitStr = value.parse()?;
+                config.primary_key = Some(lit.value());
+                Ok(())
+            } else { Err(metadata.error("Unsupported attribute type")) }
+        }).expect("TODO: panic message");
+    }
 
     let struct_identifier = &input.ident;
 
