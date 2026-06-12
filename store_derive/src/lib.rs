b33bd3bc5f94edc28842ae8c73edd763e0bc4257
Eduardo <eduardo.sbassani@gmail.com>
2026-06-12 15:47:18 +0200

    Functions for macro configs

--- store_derive/src/lib.rs
+++ store_derive/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::{TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, LitStr};
+use syn::{parse_macro_input, Attribute, Data, DeriveInput, Field, Fields, LitStr};
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
 
 #[derive(Default, Debug)]
 struct StoreConfig {
@@ -8,8 +10,9 @@ struct StoreConfig {
     primary_key: Option<String>
 }
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 struct FieldConfig {
+    ident: syn::Ident,
     column_name: Option<String>,
     primary_key: bool,
     skip: bool
@@ -21,25 +24,7 @@ pub fn derive_store(token_stream: TokenStream) -> TokenStream {
 
     let attributes = &input.attrs;
 
-    let mut config = StoreConfig::default();
-
-    for attr in attributes {
-        if !attr.path().is_ident("store") { continue; }
-
-        attr.parse_nested_meta(|metadata| {
-            if metadata.path.is_ident("primary_key") {
-                let value = metadata.value()?;
-                let lit: LitStr = value.parse()?;
-                config.table = Some(lit.value());
-                Ok(())
-            } else if metadata.path.is_ident("primary_key") {
-                let value = metadata.value()?;
-                let lit: LitStr = value.parse()?;
-                config.primary_key = Some(lit.value());
-                Ok(())
-            } else { Err(metadata.error("Unsupported attribute type")) }
-        }).expect("TODO: panic message");
-    }
+    let store_config = get_store_config(attributes);
 
     let struct_identifier = &input.ident;
 
@@ -78,4 +63,76 @@ pub fn derive_store(token_stream: TokenStream) -> TokenStream {
             }
         }
     }.into()
+}
+
+fn get_store_config(attrs: &[Attribute]) -> syn::Result<StoreConfig> {
+    let mut config = StoreConfig::default();
+
+    for attr in attrs {
+        if !attr.path().is_ident("store") { continue; }
+
+        attr.parse_nested_meta(|metadata| {
+            if metadata.path.is_ident("table") {
+                let value = metadata.value()?;
+                let lit: LitStr = value.parse()?;
+                config.table = Some(lit.value());
+                Ok(())
+            } else if metadata.path.is_ident("primary_key") {
+                let value = metadata.value()?;
+                let lit: LitStr = value.parse()?;
+                config.primary_key = Some(lit.value());
+                Ok(())
+            } else { Err(metadata.error("Unsupported attribute type")) }
+        })?;
+    }
+
+    Ok(config)
+}
+
+fn get_field_configs(fields: &Punctuated<Field, Comma>) -> syn::Result<Vec<FieldConfig>> {
+    let mut field_configs = Vec::new();
+
+    for field in fields {
+        let Some(ident) = field.ident.clone() else { continue };
+
+        let config = parse_field_config(&field.attrs, ident)?;
+
+        field_configs.push(config);
+    }
+
+    Ok(field_configs)
+}
+
+fn parse_field_config(attributes: &[Attribute], ident: syn::Ident) -> syn::Result<FieldConfig> {
+    let mut column_name: Option<String> = None;
+    let mut primary_key = false;
+    let mut skip = false;
+
+    for attr in attributes {
+        if !attr.path().is_ident("store") {
+            continue
+        }
+
+        attr.parse_nested_meta(|metadata| {
+            if metadata.path.is_ident("column_name") {
+                let value = metadata.value()?;
+                let lit: LitStr = value.parse()?;
+                column_name = Some(lit.value());
+                Ok(())
+            } else if metadata.path.is_ident("skip") {
+                skip = true;
+                Ok(())
+            } else if metadata.path.is_ident("primary_key") {
+                primary_key = true;
+                Ok(())
+            } else { Err(metadata.error("unsupported store attribute on field")) }
+        })?;
+    }
+
+    Ok(FieldConfig {
+        ident,
+        column_name,
+        primary_key,
+        skip
+    })
 }
